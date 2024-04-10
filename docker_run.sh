73e22b8d45ae752530a53a4b1f88bfa70f036291
CimimUxMaio <juanbiabdon@gmail.com>
2024-04-10 10:50:58 -0300

    Updated docker_run.sh script to load .env file on docker_run for better monitoring and added a flag --bash that allows the user to run a container in interactive mode with bash

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,19 @@
 IMAGE_NAME=$1
 CONTAINER_NAME="${2:-$IMAGE_NAME}"
 
-docker run --name $CONTAINER_NAME -d -v $(pwd)/data/:/app/data/ -v $(pwd)/.env:/app/.env $IMAGE_NAME
+OPTIONS="--name $CONTAINER_NAME -v $(pwd)/data/:/app/data/ --env-file .env"
+if ! [ -z "$3" ]; then
+    if [ "$3" == "--bash" ]; then
+        echo "Running bash in interactive mode."
+        OPTIONS="$OPTIONS --rm -it"
+        COMMAND=bash
+    else
+        echo "Unknown option: $3" 1>&2
+        exit 1
+    fi
+else
+    echo "Running application in detached mode."
+    OPTIONS="$OPTIONS -d"
+fi
+
+docker run $OPTIONS $IMAGE_NAME $COMMAND
