229b788316fdb9bbe2a8d0351e45c17bd8cf11ab
CimimUxMaio <juanbiabdon@gmail.com>
2024-04-29 22:38:20 -0300

    Add logs to scrappers

--- src/trackers/scrappers/edesur.py
+++ src/trackers/scrappers/edesur.py
@@ -1,7 +1,10 @@
 import config
+import logging
 
 from selenium.webdriver.firefox.webdriver import WebDriver
 from .types import DebtReport, Scrapper
+
+logger = logging.getLogger(__name__)
 
 
 class Edesur(Scrapper):
@@ -16,7 +19,9 @@ class Edesur(Scrapper):
         }
 
     def scrap(self, driver: WebDriver) -> list[DebtReport]:
-        driver.get(self.link + "/login")
+        base_url = f"{self.link}/login"
+        logger.info(f"Navigating to: {base_url}")
+        driver.get(base_url)
 
         email_input = driver.find_element(
             "xpath",
@@ -31,9 +36,13 @@ class Edesur(Scrapper):
             "//button[text() = 'Ingresar']",
         )
 
+        logger.info("Log in elements found")
+
         email_input.send_keys(self.login_credentials["email"])
         password_input.send_keys(self.login_credentials["password"])
         submit_button.click()
+
+        logger.info("Log in successful")
 
         def get_account_list(*, wait=False):
             account_list_button = driver.find_element(
@@ -48,20 +57,35 @@ class Edesur(Scrapper):
                 "xpath", "//selector-account//div[@class = 'mt-1 scroll-y-25']/div"
             )
 
+            logger.info("Account list found")
             return account_list
 
         account_list = get_account_list(wait=True)
 
+        account_num = len(account_list)
+        logger.info(f"Found {account_num} account elements")
+
+        if account_num == 0:
+            raise Exception("No accounts where found")
+
         reports = []
-        for i in range(len(account_list)):
+        for i in range(account_num):
             account = account_list[i]
+            logger.info(f"Scrapping account {i}")
+
+            logging.info("Navigating to account page")
             account.click()
 
+            logging.info("Waiting for account info to load")
             self.wait()  # Wait for account info to load
 
             account_data = driver.find_elements("tag name", "td")
+
             id = account_data[2].text
+            logging.info(f"ID found: {id}")
+
             address = account_data[1].text
+            logging.info(f"Address found: {address}")
 
             debt_panel = driver.find_element(
                 "xpath",
@@ -73,6 +97,8 @@ class Edesur(Scrapper):
                 ".//div[./p[text() = 'TOTAL A PAGAR']]/p[2] | .//span[contains(text(), 'no posee deuda')]",
             ).text
 
+            logging.info("Debt elements found")
+
             debt = 0
             if debt_text != "Al día de la fecha, su cuenta no posee deuda.":
                 debt = float(
@@ -82,7 +108,12 @@ class Edesur(Scrapper):
                     .replace(",", ".")
                 )
 
+            logging.info(f"Debt found: {debt}")
+
+            logging.info(f"Account {i} scrapping successful: {id}, {address}, {debt}")
+
             reports.append(DebtReport(id, address, debt))
+
             account_list = get_account_list()
 
         return reports
--- src/trackers/scrappers/metrogas.py
+++ src/trackers/scrappers/metrogas.py
@@ -1,7 +1,11 @@
 import config
+import logging
 
 from selenium.webdriver.firefox.webdriver import WebDriver
 from .types import DebtReport, Scrapper
+
+
+logger = logging.getLogger(__name__)
 
 
 class Metrogas(Scrapper):
@@ -19,20 +23,28 @@ class Metrogas(Scrapper):
         }
 
     def scrap(self, driver: WebDriver) -> list[DebtReport]:
+        logger.info(f"Navigating to: {self.link}")
         driver.get(self.link)
 
         login_button = driver.find_element("id", "loginButton")
+        logger.info("Login button found")
         login_button.click()
+        logger.info("Navigating to login page")
 
+        logger.info("Waiting for login page to load")
         self.wait()
 
         email_input = driver.find_element("id", "j_username")
         password_input = driver.find_element("id", "j_password")
         submit_button = driver.find_element("id", "logOnFormSubmit")
 
+        logger.info("Login elements found")
+
         email_input.send_keys(self.login_credentials["email"])
         password_input.send_keys(self.login_credentials["password"])
         submit_button.click()
+
+        logger.info("Login successful")
 
         def get_account_list():
             account_list_button = driver.find_element(
@@ -47,15 +59,25 @@ class Metrogas(Scrapper):
                 "//div[.//span[contains(text(), 'Seleccioná el Número de Cliente')]]//li",
             )
 
+            logger.info("Account list found")
             return account_list
 
         account_list = get_account_list()
+        account_num = len(account_list)
+        logger.info(f"Found {account_num} account elements")
+
+        if account_num == 0:
+            raise Exception("No accounts where found")
 
         reports = []
-        for i in range(len(account_list)):
+        for i in range(account_num):
+            logger.info(f"Scrapping account {i}")
             account = account_list[i]
+
+            logger.info("Navigating to account page")
             account.click()
 
+            logger.info("Waiting for account info to load")
             self.wait()  # Wait for account info to load
 
             id = driver.find_element(
@@ -63,15 +85,20 @@ class Metrogas(Scrapper):
                 "//div[./div/span[text() = 'Número de Cliente']]/div[2]//span",
             ).text
 
+            logging.info(f"ID found: {id}")
+
             address = driver.find_element(
                 "xpath",
                 "//div[./div/span[text() = 'Dirección']]/div[2]//span",
             ).text
+            logging.info(f"Address found: {address}")
 
             debt_text = driver.find_element(
                 "xpath",
                 "//div[./div/div[text() = 'Estado de Cuenta']]//span[contains(text(), '$') or text() = 'No registra deuda']",
             ).text
+
+            logging.info("Debt elements found")
 
             debt = 0
             if debt_text != "No registra deuda":
@@ -81,6 +108,10 @@ class Metrogas(Scrapper):
                     .replace(".", "")
                     .replace(",", ".")
                 )
+
+            logging.info(f"Debt found: {debt}")
+
+            logging.info(f"Account {i} scrapping successful: {id}, {address}, {debt}")
 
             reports.append(DebtReport(id, address, debt))
 
